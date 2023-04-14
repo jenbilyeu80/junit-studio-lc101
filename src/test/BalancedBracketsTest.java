03ddb3cf5dfb9ecffc18d5a0ae8e6a83fea190ff
Jenbilyeu80 <jenbilyeu@yahoo.com>
2023-04-13 19:37:54 -0500

    studioTest

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,43 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    // @Test public void emptyTest() {
+    void assertEquals() {
+
     }
 
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void TwoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[] []"));
+    }
+
+    @Test
+    public void BracketsWithinReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void twoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]] []"));
+    }
+
+    @Test
+    public void oneBracketWithinReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+
+
+    @Test
+    public void twoGroupBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[] []]]"));
+
+    }
 }
+
